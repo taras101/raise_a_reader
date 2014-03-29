f17a9b1da1ff854e2b54dde1de945512a816973b
Taras101 <taras101@gmail.com>
2014-03-29 09:17:01 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RaiseAReader::Application.config.secret_key_base = '482cdce1e85c3db2c02f6c2dd342b2a0ff279d9ea6fd5ff1d5c2f2fdbc5f2013562b0d2db584b6aa993d2e9edf79c17d4cf42722eb2c1790b0314f4eacae1b10'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RaiseAReader::Application.config.session_store :cookie_store, key: '_raise_a_reader_session'
